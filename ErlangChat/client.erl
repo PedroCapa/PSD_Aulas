bfc8b939f8ee22ec0022b5d7459069add70b84d2
PedroCapa <pmccapa@hotmail.com>
2019-11-11 21:39:32 +0000

    Erlang Chat

---
+++ ErlangChat/client.erl
@@ -0,0 +1,19 @@
+-module (client).
+-export ([client/1, sendData/2]).
+
+client(Port) -> %Enviar por argumento o PID do servidor
+		{ok, Sock} = gen_tcp:connect("127.0.0.1", Port, [binary, {active,false}]),
+		io:format("Entrei no servidor ~n"),
+		spawn(fun() -> receiveData(Sock) end),
+		sendData(Port, Sock).
+
+sendData(Port, Sock) ->
+	    Term = io:get_line("message: "),
+		ok = gen_tcp:send(Sock, Term),
+	    sendData(Port, Sock).
+
+
+receiveData(Sock) ->
+		{ok, Term} = gen_tcp:recv(Sock, 0),
+	    io:format("Client received: ~s~n", [Term]),
+	    receiveData(Sock).
