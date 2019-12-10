6d06736f56e268db849964f88557205ba05ad08e
LFCC98 <a81960@alunos.uminho.pt>
2019-12-10 23:04:30 +0000

    ZeroMQ

---
+++ ZeroMQ/src/main/java/ZeroMQ/Cliente.java
@@ -0,0 +1,32 @@
+//  Hello World client in Java
+//  Connects REQ socket to tcp://localhost:5555
+//  Sends "Hello" to server, expects "World" back
+import java.net.*;
+import java.io.*;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+import java.util.Random;
+
+public class Cliente
+{
+    public static void main(String[] args)
+    {
+        try (ZContext context = new ZContext()) {
+            System.out.println("Connecting to hello world server");
+
+      		//  Socket to talk to server
+            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
+            subscriber.connect("tcp://localhost:5555");
+
+            Random rand = new Random();
+            int id = rand.nextInt(10);
+
+            subscriber.subscribe(String.format("%05d", id));
+            String reply = subscriber.recvStr();
+            System.out.println(reply);
+        }
+    }
+}
---
+++ ZeroMQ/src/main/java/ZeroMQ/Server.java
@@ -0,0 +1,25 @@
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+import java.util.Random;
+
+public class Server
+{
+    public static void main(String[] args) throws Exception
+    {
+        try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket publisher = context.createSocket(ZMQ.PUB);
+            publisher.bind("tcp://*:5555");
+
+            Random rand = new Random();
+
+            while (!Thread.currentThread().isInterrupted()) {
+                //Send to the subscribers
+                int id = rand.nextInt(10);
+                int data = rand.nextInt(100);
+                publisher.send(String.format("%05d %d", id, data));
+            }
+        }
+    }
+}
